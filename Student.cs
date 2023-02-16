eadbbb60f5ce80cdec916d29697f5b945acb4082
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-02-16 15:00:40 +0300

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,13 @@
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Mobile { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+}

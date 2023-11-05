7357c6b6be92accd62a61bee55d74e15e1c00759
nirzaf <mfmfazrin1986@gmail.com>
2023-11-05 22:24:28 +0300

    Add initial project files and Employee class
    In this commit, we initialize the TinkeringApp project. This includes the addition of configuration and project files, such as global.json and TinkeringApp.sln. A .gitignore is also added to avoid tracking unnecessary files.
    
    Specifically, a new class "ValidQId" is introduced to validate the QId string inserted via the console input. This helps ensure that only numerical string values are valid for the QId property of the new Employee class. This is needed to ensure data consistency and integrity in the application.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using TinkeringApp;
+
+
+ Console.WriteLine("Enter employee ID");
+ var employeeId = Console.ReadLine();
+ 
+Console.WriteLine("Enter employee name");
+var employeeName = Console.ReadLine();
+ 
+    var employee = new Employee
+    {
+        QId = employeeId,
+        Name = employeeName
+    };
+
+   Console.WriteLine($"Employee ID: {employee.QId}");
+ 
+ 
+ public class Employee
+ {
+        [ValidQId]
+        public string? QId { get; set; }
+        
+        public string? Name { get; set; }
+ }
+
+ 

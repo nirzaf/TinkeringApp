84b0f68d4079a2b1d2500cbb65182cb0eb4e6bcd
nirzaf <mfmfazrin1986@gmail.com>
2023-11-07 19:02:47 +0300

    Remove ValidQId.cs and add CalculateScore.cs
    ValidQId.cs was deleted due to unknown reasons. A new file named CalculateScore.cs was created, which includes characteristics like Score, AddScore, and GetScore. Modifications were also made in Program.cs, changing the functionalities and implementing a distance calculation between two geographical points.

---
+++ CalculateScore.cs
@@ -0,0 +1,36 @@
+﻿
+public record struct CalculateScore
+{
+    private int Score { get; set; }
+
+    public CalculateScore(int score)
+    {
+        Score = score;
+    }
+
+    public void AddScore(int score)
+    {
+        Score += score;
+    }
+    
+    public int GetScore()
+    {
+        return Score;
+    }
+}
+
+record struct GlobalConstants
+{
+    public const int MaxScore = 100;
+    public const int MinScore = 0;
+}
+
+public static class GlobalConstant
+{
+    public const int MaxScore = 100;
+    public const int MinScore = 0;
+}
+
+public record struct Location(double Latitude, double Longitude);
+
+
--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
-﻿using TinkeringApp;
+﻿Console.WriteLine("Enter your marks :");
+
+//calculate between two points
+
+double lat1 = 52.2296756;
+double lon1 = 21.0122287;
+
+double lat2 = 55.406374;
+double lon2 = 16.9251681;
+
+double distance = CalculateDistance(lat1, lon1, lat2, lon2);
+
+Console.WriteLine($"Distance between two points is {distance} meters");
+
+// show in km
+Console.WriteLine($"Distance between two points is {distance/1000} km");
 
 
- Console.WriteLine("Enter employee ID");
- var employeeId = Console.ReadLine();
- 
-Console.WriteLine("Enter employee name");
-var employeeName = Console.ReadLine();
- 
-    var employee = new Employee
-    {
-        QId = employeeId,
-        Name = employeeName
-    };
+static double CalculateDistance(double lat1, double lon1, double lat2, double lon2) 
+{
+    double R = 6371e3; // Earth's radius in m
+    double φ1 = lat1 * (Math.PI/180); // Convert degrees to radians
+    double φ2 = lat2 * (Math.PI/180);
+    double Δφ = (lat2-lat1) * (Math.PI/180);
+    double Δλ = (lon2-lon1) * (Math.PI/180);
 
-   Console.WriteLine($"Employee ID: {employee.QId}");
- 
- 
- public class Employee
- {
-        [ValidQId]
-        public string? QId { get; set; }
-        
-        public string? Name { get; set; }
- }
+    double a = Math.Sin(Δφ/2) * Math.Sin(Δφ/2) +
+               Math.Cos(φ1) * Math.Cos(φ2) *
+               Math.Sin(Δλ/2) * Math.Sin(Δλ/2);
+    double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a));
 
- 
+    double distanceInMeters = R * c;
+    return distanceInMeters;
+}
